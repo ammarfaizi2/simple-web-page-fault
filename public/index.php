5b016fd3033ba8d2c69be418310518fbf469f995
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-02 14:40:57 +0700

    Fix index page fault
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- public/index.php
+++ public/index.php
@@ -9,23 +9,37 @@
 	<script type="text/javascript" src="assets/js/base.js"></script>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/category.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/item.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/base.css"/>
 </head>
 <body>
 	<?php require __DIR__."/navbar_main.php"; ?>
-	<div class="prod-cont">
-		<h1 id="dafter-menu">Daftar Menu Semua Kategori</h1>
+	<div class="container prod-cont">
+		<nav aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<a href="index.php"><li class="breadcrumb-item active" aria-current="page">Home</li></a>
+			</ol>
+		</nav>
+		<h1 id="menu-heading">Daftar Menu Semua Kategori</h1>
+		<div class="container">
+			<div class="row">
 <?php foreach (loadItems(__DIR__."/items/list.txt") as $k => $it): ?>
-		<a href="item.php?id=<?= e($k); ?>">
-			<div class="pt">
-				<img class="prod" src="<?= e($it["img"]) ?>"/>
-				<div class="prod-sdesc"><?= e($it["title"]) ?></div>
-				<div class="prod-price">
-					<p>Rp.<?= e(number_format($it["price"], 0, ",", ".")) ?></p>
-				</div>
+			<div class="col">
+				<a href="item.php?id=<?= e($k); ?>">
+					<div class="card prod-card" style="width: 18rem;">
+						<img class="prod" src="<?= e($it["img"]) ?>" alt="<?= e($it["title"]) ?>"/>
+						<div class="card-body prod-title">
+							<p class="card-text"><?= e($it["title"]) ?></p>
+						</div>
+						<div class="card-body prod-title">
+							<p class="card-text">Rp.<?= e(number_format($it["price"], 0, ",", ".")) ?></p>
+						</div>
+					</div>
+				</a>
 			</div>
-		</a>
 <?php endforeach; ?>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
