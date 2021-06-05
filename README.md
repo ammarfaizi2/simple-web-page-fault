87c0a62802eadea3581d47a9952f7bb6adf544cb
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-05 15:59:12 +0700

    Add README.md and fix item page
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Web Page Fault
+
--- public/item.php
+++ public/item.php
@@ -7,18 +7,50 @@ if (!isset($_GET["id"])) {
 
 require __DIR__."/func.php";
 
+$id = $_GET["id"];
+
+
+$bdir = __DIR__."/items/{$id}";
+if (!is_dir($bdir)) {
+	echo "Invalid ID {$id}";
+	exit(1);
+}
+
+$title = file_get_contents($bdir."/title.txt");
+$price = file_get_contents($bdir."/price.txt");
+$desc  = file_get_contents($bdir."/desc.txt");
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Kategori Wanita</title>
+	<title><?= e($title) ?></title>
 	<script type="text/javascript" src="assets/js/jquery-3.4.1.slim.min.js"></script>
 	<script type="text/javascript" src="assets/js/popper.min.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/item.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/base.css"/>
 </head>
 <body>
-	<?php require __DIR__."/navbar_main.php"; ?>
+	<?php require __DIR__."/navbar.php"; ?>
+	<div class="prod-cont">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<img class="img-itct" src="items/<?= e($id) ?>/image.jpg"/>
+				</div>
+				<div class="col">
+					<div class="item-info">
+						<h3><?= e($title) ?></h3>
+						<p><?= str_replace("\n", "<br/>", e($desc)) ?></p>
+						<div>
+							<button class="btn btn-primary">Beli</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
