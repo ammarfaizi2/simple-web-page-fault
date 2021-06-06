791da698d8c650943f85b1bd829d210265702ff5
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-06 22:37:42 +0700

    Small fix up
    Co-authored-by: Fahmi
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ public/footer.php
@@ -0,0 +1,35 @@
+<div class="wave">
+	<img src="/assets/wave.svg" alt="">
+</div>
+<footer class="footer-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 item text">
+				<h3>KLEREN</h3>
+				<p>Fashion di Indonesia kini semakin berkembang mengikuti arus modernisasi. Perkembangan ini menjadikan masyarakat sebagai masyarakat yang selektif di dalam menentukan gaya hidupnya. Gaya hidup sangat erat hubungannya dengan fashion, Produk fashion meliputi pakaian, sepatu, tas, aksesoris, dan lain sebagainya. Dengan adanya masalah ini, kami memiliki sebuah ide untuk membuat sebuah web fashion dengan nama KLEREN, dimana kleren ini kami ambil dari bahasa belanda yang artinya pakaian.</p>
+			</div>
+			<div class="col-sm-6 col-md-3 item" style="margin-left: 20px;">
+				<h3>LAYANAN</h3>
+				<ul>
+					<li><a href="#">Bantuan</a></li>
+					<li><a href="#">Cek Order</a></li>
+					<li><a href="#">Cara Pengembalian</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-3 item">
+				<h3>TENTANG KAMI</h3>
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Kebijakan Privasi</a></li>
+				</ul>
+			</div>
+			<div class="col item social">
+				<a href="#"><i class="icon ion-social-facebook"></i></a>
+				<a href="#"><i class="icon ion-social-twitter"></i></a>
+				<a href="#"><i class="icon ion-social-instagram"></i></a>
+			</div>
+		</div>
+		<p class="copyright">Company Name © 2021</p>
+	</div>
+</footer>
--- public/navbar.php
+++ public/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="index.php">Kleren</a>
+		<a class="navbar-brand" href="index.php" style="margin-left: 100px;">Kleren</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
